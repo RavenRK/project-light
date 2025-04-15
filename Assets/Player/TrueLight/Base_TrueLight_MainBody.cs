45d3b6f6b1b944c8c621bb9313aa5174eeb39628
RavenRK <jamesjiang.workemail@gmail.com>
2025-04-15 03:59:10 +0100

    e

--- Assets/Player/TrueLight/Base_TrueLight_MainBody.cs
+++ Assets/Player/TrueLight/Base_TrueLight_MainBody.cs
@@ -1,13 +1,15 @@
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-public class Base_TrueLight : MonoBehaviour
+public class Base_TrueLight_MainBody : MonoBehaviour
 {
     [Header("TrueLight settings")]
     public float orbitSpeed = 10f;         // Speed of the orbit
-    public float AttackPower = 10f;         // Attack power << throw power >>
-    public Transform playerCamera;
-    public Transform originLocation;
+    public float AttackPower = 10f;        // Attack power << throw power >>
+    public Transform originLocation;       //attack point and recall point for projectile
+
+    public float projectileForce = 10f;    // Speed of the projectile
+    public GameObject projectilePrefab;    // Prefab of the projectile
 
     private Rigidbody rb;
 
@@ -31,14 +33,17 @@ public class Base_TrueLight : MonoBehaviour
     {
         if (context.performed)
         {
-            Vector3 forward = playerCamera.forward;                                                      // Get the forward and right vectors of the camera
-            rb.AddForce(forward * AttackPower, ForceMode.Impulse);        // Apply force to the TrueLight
+            GameObject proj = Instantiate(projectilePrefab, originLocation.position, originLocation.rotation);
+            Rigidbody rb = proj.GetComponent<Rigidbody>();
+            rb.AddForce(originLocation.forward * projectileForce, ForceMode.Impulse);
         }
     }
     public void OnRecall(InputAction.CallbackContext context)
     {
         if (context.performed)
         {
+            //make vis
+            //recall proj
             transform.position = originLocation.position;         // Recall the TrueLight to its original location
             rb.linearVelocity = Vector3.zero;                     // Stop the TrueLight
         }
---
+++ Assets/Player/TrueLight/LightProJ.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class LightProJ : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+    void Update()
+    {
+        
+    }
+}
