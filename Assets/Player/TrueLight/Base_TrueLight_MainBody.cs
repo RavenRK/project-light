90786cb89df7cc21f20792ed1ce4867c2b2d419f
RavenRK <jamesjiang.workemail@gmail.com>
2025-04-15 07:59:41 +0100

    lights

--- Assets/Player/TrueLight/Base_TrueLight_MainBody.cs
+++ Assets/Player/TrueLight/Base_TrueLight_MainBody.cs
@@ -3,49 +3,68 @@ using UnityEngine.InputSystem;
 
 public class Base_TrueLight_MainBody : MonoBehaviour
 {
+    #region <_TrueLight settings >
     [Header("TrueLight settings")]
     public float orbitSpeed = 10f;         // Speed of the orbit
     public float AttackPower = 10f;        // Attack power << throw power >>
-    public Transform originLocation;       //attack point and recall point for projectile
-
     public float projectileForce = 10f;    // Speed of the projectile
-    public GameObject projectilePrefab;    // Prefab of the projectile
+
+    [Header("Transform settings")]
+    public Transform originLocation;       //attack point and recall point for projectile
+    public Transform forwardDir;           // Direction of the projectile should go
+
+    [Header("Prefab settings")]
+    public GameObject projectilePrefab;   // Prefab of the projectile
+    public GameObject pointLight;         // Reference to the point light object
+    private GameObject projectile;        // Reference to the projectile object
 
     private Rigidbody rb;
-
+    private Renderer Renderer;             // Reference to the renderer component
+    public bool CanFire = true;           // Check if the projectile can be fired
+    #endregion
     void Awake()
     {
         rb = GetComponent<Rigidbody>();
+        Renderer = GetComponent<Renderer>(); // Get the renderer component
     }
-    void Start()
-    {
+    private void FixedUpdate() { orbit(orbitSpeed); }
 
-    }
-    private void FixedUpdate()
-    {
-        orbit(orbitSpeed);
-    }
-    void orbit(float speed)
-    {
-        transform.Rotate(speed * Time.deltaTime, 0, speed * Time.deltaTime); // Rotate around the target point
-    }
     public void OnAttack(InputAction.CallbackContext context)
     {
-        if (context.performed)
+        if (context.performed && CanFire)
         {
-            GameObject proj = Instantiate(projectilePrefab, originLocation.position, originLocation.rotation);
-            Rigidbody rb = proj.GetComponent<Rigidbody>();
-            rb.AddForce(originLocation.forward * projectileForce, ForceMode.Impulse);
+            projectile = Instantiate(projectilePrefab, originLocation.position, originLocation.rotation);
+
+            Rigidbody rb = projectile.GetComponent<Rigidbody>();
+            rb.AddForce(forwardDir.forward * projectileForce, ForceMode.Impulse);
+            SetState(false);
         }
     }
     public void OnRecall(InputAction.CallbackContext context)
     {
         if (context.performed)
+            projectile.GetComponent<LightProJ>().StartRecall(this.transform); // pass player position
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        LightProJ proj = other.GetComponent<LightProJ>();
+        if (proj.IsRecalling())
         {
-            //make vis
-            //recall proj
-            transform.position = originLocation.position;         // Recall the TrueLight to its original location
-            rb.linearVelocity = Vector3.zero;                     // Stop the TrueLight
+            SetState(true);               // Enable the TrueLight when the projectile is recalled
+            proj.CompleteRecall();
         }
     }
+
+    public void OnRecall() { SetState(true); }
+    public void SetState(bool state)
+    {
+        Renderer.enabled = state;        // Enable the renderer of the TrueLight
+        pointLight.SetActive(state);     // Enable the point light object
+        CanFire = state;                 // Set the CanFire flag to true to allow firing again
+    }
+    void orbit(float speed)
+    {
+        transform.Rotate(speed * Time.deltaTime, 0, speed * Time.deltaTime); // Rotate around the target point
+    }
+
 }
--- Assets/Player/TrueLight/LightProJ.cs
+++ Assets/Player/TrueLight/LightProJ.cs
@@ -2,12 +2,40 @@ using UnityEngine;
 
 public class LightProJ : MonoBehaviour
 {
-    void Start()
+    private Transform returnLocation;   // The location to return to
+    private bool isRecalling = false;   // Check if the projectile is recalling
+    public float recallSpeed = 10f;     // Speed of the recall
+
+    private int bounceCount = 0;         // Count the number of bounces
+    private int maxBounces = 3;            // Maximum number of bounces
+    public PhysicsMaterial nonBouncyMaterial;
+
+    private Rigidbody rb;
+    void Awake() { rb = GetComponent<Rigidbody>(); }
+    private void FixedUpdate()
     {
-        
+        if (isRecalling)
+            transform.position = Vector3.MoveTowards(transform.position, returnLocation.position, recallSpeed * Time.deltaTime);
     }
-    void Update()
+
+    public void StartRecall(Transform target)
     {
-        
+        returnLocation = target;            // Set the return location to the target
+        isRecalling = true;                 // Set the recalling flag to true
+
+        rb.isKinematic = true;              // Disable physics if needed
+        rb.linearVelocity = Vector3.zero;   // Stop the projectile
+    }
+    public void CompleteRecall()
+    {
+        isRecalling = false;
+        Destroy(gameObject);
+    }
+    public bool IsRecalling() => isRecalling;
+    void OnCollisionEnter(Collision collision)
+    {
+        bounceCount++;
+        if (bounceCount >= maxBounces)
+            GetComponent<Collider>().material = nonBouncyMaterial;
     }
 }
