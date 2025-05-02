63b36dc21ec4584dc4375951c3b4e100f7518074
Raven <jamesjiang.workemail@gmail.com>
2025-05-02 10:36:58 +0100

    Revert "mini proJ"
    This reverts commit a23ec99f3ad664529bd67161570783b384d4825d.

--- Assets/Player/TrueLight/LightProJ.cs
+++ Assets/Player/TrueLight/LightProJ.cs
@@ -4,19 +4,18 @@ public class LightProJ : MonoBehaviour
 {
     private Transform returnLocation;   // The location to return to
     private bool isRecalling = false;   // Check if the projectile is recalling
-    public float recallSpeed = 5;       // Speed of the recall
-    public GameObject miniLight;        // Reference to the mini light prefab
+    public float recallSpeed = 5;     // Speed of the recall
 
-    private int bounceCount = 0;        // Count the number of bounces
-    private int maxBounces = 3;         // Maximum number of bounces
+    private int bounceCount = 0;         // Count the number of bounces
+    private int maxBounces = 3;            // Maximum number of bounces
     public PhysicsMaterial nonBouncyMaterial;
 
     private Rigidbody rb;
-    void Awake() { rb = GetComponent<Rigidbody>(); }    // Get the rigidbody component
+    void Awake() { rb = GetComponent<Rigidbody>(); }
     private void FixedUpdate()
     {
         if (isRecalling)
-            transform.position = Vector3.MoveTowards(transform.position, returnLocation.position, recallSpeed * Time.deltaTime); // Move towards the return location (player)
+            transform.position = Vector3.MoveTowards(transform.position, returnLocation.position, recallSpeed * Time.deltaTime);
     }
 
     public void StartRecall(Transform target)
@@ -24,26 +23,19 @@ public class LightProJ : MonoBehaviour
         returnLocation = target;            // Set the return location to the target
         isRecalling = true;                 // Set the recalling flag to true
 
-        rb.isKinematic = true;              // Disable physics
+        rb.isKinematic = true;              // Disable physics if needed
         rb.linearVelocity = Vector3.zero;   // Stop the projectile
     }
     public void CompleteRecall()
     {
-        isRecalling = false;    // Set the recalling flag to false
-        Destroy(gameObject);    // Destroy the projectile
+        isRecalling = false;
+        Destroy(gameObject);
     }
-    public bool IsRecalling() => isRecalling;   // Check if the projectile is recalling 
-    void OnCollisionEnter(Collision collision) 
+    public bool IsRecalling() => isRecalling;
+    void OnCollisionEnter(Collision collision)
     {
-        bounceCount++;  // Increment the bounce count
+        bounceCount++;
         if (bounceCount >= maxBounces)
             GetComponent<Collider>().material = nonBouncyMaterial;
-
-        // Get the contact point
-        Vector3 spawnPoint = collision.contacts[0].point;
-
-        // Spawn mini light
-        GameObject mini = Instantiate(miniLight, spawnPoint, Quaternion.identity);
-
     }
 }
