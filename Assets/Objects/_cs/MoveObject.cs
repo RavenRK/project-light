f3ca6815810264b870ab0aa254bfd30809e97cf8
RavenRK <jamesjiang.workemail@gmail.com>
2025-05-09 01:51:06 +0100

    thing

--- Assets/Objects/_cs/MoveObject.cs
+++ Assets/Objects/_cs/MoveObject.cs
@@ -1,62 +1,38 @@
+using NUnit.Framework.Constraints;
+using TMPro;
 using UnityEditor.Rendering;
 using UnityEngine;
 
 public class MoveObject : BasePowerUser
 {
     [Header("Move Settings")]
-    public Vector3 moveOffset;
-    public float Xmove, Zmove;
     public float moveSpeed = 2f;
+    public Vector3 targetPosition;
+    public GameObject Player;
 
     private Vector3 originalPosition;
-    private Vector3 targetPosition;
-
-    private Rigidbody collidingObject = null;
     private Rigidbody rb;
     
-
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         originalPosition = transform.position;
-        targetPosition = originalPosition;
     }
 
     void Update()
     {
-        if (base.PoweredObj == base.powerNeeded)
+        if (PoweredObj == powerNeeded)
         {
-            // rb.linearVelocity = new Vector3(Xmove,0,Zmove);
-            transform.position += new Vector3(Xmove, 0, Zmove) * Time.deltaTime ;
-
-            Debug.Log(collidingObject);
-
-            if (collidingObject != null)
-            {
-                collidingObject.GetComponent<movementPlayer>().UpdateIsMoving(new Vector3(Xmove, 0, Zmove));
-                collidingObject.linearVelocity = new Vector3(Xmove, 0, Zmove);
-            }
-
+            transform.position = Vector3.MoveTowards(transform.position, targetPosition, moveSpeed * Time.deltaTime);
         }
-        else if(collidingObject != null)
-            collidingObject.GetComponent<movementPlayer>().UpdateIsMoving(new Vector3(0, 0, 0));
-    }
-    public override void OnPowered() { targetPosition = originalPosition + moveOffset; base.PoweredObj += 1; }
-    public override void OffPowered() { targetPosition = originalPosition; PoweredObj = 0; }
-    
-    void OnTriggerEnter(Collider collision)
-    {
-        if (collision.gameObject.CompareTag("Player"))
+        else if (originalPosition != transform.position && PoweredObj != powerNeeded)
         {
-            collidingObject = collision.GetComponent<Rigidbody>();
-            //collision.transform.SetParent(transform);
-            //Debug.Log("Player is now a child of the moving object");
+            transform.position = Vector3.MoveTowards(transform.position, originalPosition, moveSpeed * Time.deltaTime);
         }
+
     }
-    private void OnTriggerExit(Collider collision)
-    {
-        //if (collision.gameObject.CompareTag("Player"))
-            //collision.transform.SetParent(null);
-    }
+
+    public override void OnPowered() {PoweredObj += 1; }
+    public override void OffPowered() {PoweredObj -= 1; }
 }
 
---
+++ Assets/Player/TrueLight/ProJControler.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ProJControler : MonoBehaviour
+{
+    public float curveStrength = 50f;
+    public float maxSpeed = 20f;
+
+    private Rigidbody rb;
+    private Camera mainCamera;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        mainCamera = Camera.main;
+    }
+
+    void FixedUpdate()
+    {
+        if (Mouse.current.leftButton.isPressed)
+        {
+            Vector3 camForward = mainCamera.transform.forward;
+            rb.AddForce(camForward.normalized * curveStrength, ForceMode.Acceleration);
+
+            // Clamp speed
+            if (rb.linearVelocity.magnitude > maxSpeed)
+                rb.linearVelocity = rb.linearVelocity.normalized * maxSpeed;
+        }
+    }
+}
